e5cb9b606c606e7914507ae29f022724969feb41
caracheng <ccheng@macalester.edu>
2014-04-14 11:37:30 -0500

    Updated

--- src/edu/macalester/comp124/mymap/MyMap.java
+++ src/edu/macalester/comp124/mymap/MyMap.java
@@ -1,5 +1,6 @@
 package edu.macalester.comp124.mymap;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -43,12 +44,34 @@ public class MyMap <K, V> {
 	 * @param key
 	 * @param value
 	 */
+
+    /*A single key / value pair is stored in a MyEntry object.
+    MyEntry is a generic type with type parameters for the key and the value: MyEntry<K, V>.
+    A single hash bucket is a list of MyEntry objects: List<MyEntry<K, V>>
+    The type for the array of hash buckets is a simple extension of the previous data structure: List<MyEntry<K, V>> []*/
+
 	public void put(K key, V value) {
-		expandIfNecessary();
-		
+		//Complete the put() method:
+       // Hint: the key may already exist!
+       //         Hint: every Java Object has a hashCode() and equals() method.
+       //         After you complete the put() method, the first unit test in TestMyMap should pass.
 		// TODO: Store the key.
+        expandIfNecessary();
+        int compressedHashCode = key.hashCode() % buckets.length;
+        List<MyEntry<K,V>> listInBucket = buckets[compressedHashCode];
+        for(int i = 0; i < listInBucket.size(); i++)
+        {
+            if(listInBucket.get(i).getKey().equals(key)) //if key already exists
+            {
+                listInBucket.get(i).setValue(value);
+                return;
+            }
+        }
+        MyEntry<K,V> newEntry = new MyEntry<>(key,value); //creates a new entry
+        listInBucket.add(newEntry);
+        numEntries++;
 	}
-	
+
 	/**
 	 * Returns the value associated with the specified key, or null if it
 	 * doesn't exist.
@@ -58,7 +81,18 @@ public class MyMap <K, V> {
 	 */
 	public V get(K key) {
 		// TODO: retrieve the key.
-		return null;
+        int compressedHashCode = Math.abs(key.hashCode()) % buckets.length;
+        V value = null;
+        List<MyEntry<K,V>> listInBucket = buckets[compressedHashCode];
+        for (int i = 0; i < listInBucket.size(); i++)
+        {
+            MyEntry<K,V> entry = listInBucket.get(i);
+           if(listInBucket.get(i).getKey().equals(key))
+           {
+               return entry.getValue();
+           }
+        }
+       return value;
 	}
 	
 	/**
@@ -66,8 +100,31 @@ public class MyMap <K, V> {
 	 */
 	private void expandIfNecessary() {
 		// TODO: expand if necessary
+
+        if(numEntries > buckets.length * loadFactor)
+        {
+            int newSize = buckets.length * 2;
+            List<MyEntry<K, V>> [] newBuckets = newArrayOfEntries(newSize); //make new array //for each array slot, make new list for each slot
+            for(int i = 0; i < buckets.length; i++) //loops through each bucket in old array
+            {
+                List<MyEntry<K,V>> aList = buckets[i]; //grabs list from old bucket
+                for(int j = 0; j < aList.size(); j++) //loops through the list
+                {
+                    K key = aList.get(j).getKey(); //grabs the key
+                    V value = aList.get(j).getValue(); //grabs value of key
+                    int compressedHashCode = Math.abs(key.hashCode()) % newBuckets.length; //compresses the hashcode of the key
+                    List<MyEntry<K,V>> listInBucket = newBuckets[compressedHashCode]; //makes a list in slot of new array
+                    MyEntry<K,V> newEntry = new MyEntry<>(key,value); //makes a new entry
+                    listInBucket.add(newEntry); //shoves the entry into the list in slot of new array
+                }
+            }
+            System.out.println("done making new buckets, making it the main buckets");
+            buckets = newBuckets; //copies over
+            System.out.println("new # buckets = "+buckets.length);
+        }
 	}
-	
+
+
 	/**
 	 * Returns an array of the specified size, each
 	 * containing an empty linked list that can be
